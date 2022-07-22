56cab80743ada07d30f83cc5301618ca27363f19
dkxce <25371256+dkxce@users.noreply.github.com>
2022-07-22 15:46:01 +0300

    Add files via upload

---
+++ device_onoff/Conso1e.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Threading;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+
+namespace device_onoff
+{
+    internal class Conso1e
+    {
+        private static Thread inputThread;
+        private static AutoResetEvent getInput, gotInput;
+        private static string input;
+
+        static Conso1e()
+        {
+            getInput = new AutoResetEvent(false);
+            gotInput = new AutoResetEvent(false);
+            inputThread = new Thread(reader);
+            inputThread.IsBackground = true;
+            inputThread.Start();
+        }
+
+        private static void reader()
+        {
+            while (true)
+            {
+                getInput.WaitOne();
+                //input = Console.ReadLine(); // normally
+
+                /* FOR ANY abracadabra */
+                ConsoleKeyInfo key = Console.ReadKey();
+                if (!char.IsControl(key.KeyChar))
+                    input += key.KeyChar.ToString();
+                /* FOR ANY abracadabra */
+
+                gotInput.Set();
+            }
+        }
+
+        public static string ReadLine()
+        {
+            return Console.ReadLine();
+        }
+
+        public static string ReadLine(int timeOutMillisecs)
+        {
+            getInput.Set();
+            bool success = gotInput.WaitOne(timeOutMillisecs);
+            if (success)
+                return input;
+            else
+                return "";
+        }
+    }
+
+    internal static class ProcWindows
+    {
+        [DllImport("user32.dll")]
+        static extern bool SetForegroundWindow(IntPtr hWnd);
+
+        [DllImport("user32.dll")]
+        private static extern IntPtr GetForegroundWindow();
+
+        [DllImport("user32.dll")]
+        private static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool TerminateProcess(IntPtr hProcess, uint uExitCode);
+
+        public static void ShowLaunched()
+        {
+            string currProcName = System.IO.Path.GetFileNameWithoutExtension(System.Reflection.Assembly.GetEntryAssembly().Location);
+            Process[] procs = Process.GetProcessesByName(currProcName);
+            foreach (Process proc in procs)
+                if ((proc.MainWindowHandle != IntPtr.Zero) && (proc.MainWindowHandle != Process.GetCurrentProcess().MainWindowHandle))
+                {
+                    ShowWindow(proc.MainWindowHandle, 5);
+                    SetForegroundWindow(proc.MainWindowHandle);
+                    break;
+                };
+        }
+
+        public static void Kill()
+        {
+            string currProcName = System.IO.Path.GetFileNameWithoutExtension(System.Reflection.Assembly.GetEntryAssembly().Location);
+            Process[] procs = Process.GetProcessesByName(currProcName);
+            foreach (Process proc in procs)
+                if ((proc.MainWindowHandle != IntPtr.Zero) && (proc.MainWindowHandle != Process.GetCurrentProcess().MainWindowHandle))
+                {
+                    try { TerminateProcess(proc.Handle, 0); } catch {};
+                    try { proc.Kill(); } catch { };
+                    break;
+                };
+        }
+    }
+}
---
+++ device_onoff/Program.cs
@@ -0,0 +1,106 @@
+﻿//
+// Author: https://github.com/dkxce
+// .Net 2.0+
+// WinXP+
+// 
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using Microsoft.Win32;
+
+namespace device_onoff
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Device On/Off");
+            Console.WriteLine("usage: device_onoff.exe");
+            Console.WriteLine("usage: device_onoff.exe /setup");
+            Console.WriteLine("   Or type any data in cosole to set DeviceID");
+
+            string ln = Conso1e.ReadLine(500);
+            if (!string.IsNullOrEmpty(ln))
+            {
+                SetDevice();
+                return;
+            };
+
+            if ((args != null) && (args.Length > 0) && (args[0] == "/setup"))
+                SetDevice();                
+            else
+                ToggleDevice();
+        }
+
+        static void ToggleDevice()
+        {
+            string DeviceID = null;
+            bool enabled = false;
+
+            ReadRegistry(ref DeviceID, ref enabled);
+            if(string.IsNullOrEmpty(DeviceID)) { SetDevice(); return; };
+
+            Console.Write("Switching {0} {1} ... ", enabled ? "off" : "on", DeviceID);
+            EnableDevice(DeviceID, enabled = !enabled);
+            WriteRegistry(DeviceID, enabled);
+            Console.WriteLine("OK");
+            string ln = Conso1e.ReadLine(1000);
+            if (!string.IsNullOrEmpty(ln))
+                SetDevice();
+        }
+
+        static void SetDevice()
+        {
+            List<string> Devices = DeviceSwitcher.ListDevices();
+            string DeviceID = "";
+            bool enabled = false;
+            ReadRegistry(ref DeviceID, ref enabled);
+            InputBox.defWidth = 500;
+            InputBox.pShowInTaskBar = true;
+            if (InputBox.Show("Select Device", "DeviceiD (from Device Manager):", Devices.ToArray(), ref DeviceID, true) != DialogResult.OK) return;
+            DeviceID = DeviceID.Trim();
+            if (string.IsNullOrEmpty(DeviceID)) return;
+            WriteRegistry(DeviceID, true);
+        }
+
+        static void EnableDevice(string DeviceID, bool enable)
+        {
+            //Guid mouseGuid = new Guid("{745a17a0-74d3-11d0-b6fe-00a0c90f57da}");
+            //string DeviceID = @"HID\ELAN072B&COL03\4&F53F679&1&0002";
+            DeviceSwitcher.SetDeviceEnabled(DeviceID, enable);
+        }
+
+        static void ReadRegistry(ref string DeviceID, ref bool enabled)
+        {
+            try
+            {
+                RegistryKey bkx86 = RegistryKey.OpenRemoteBaseKey(RegistryHive.CurrentUser, Environment.MachineName);
+                RegistryKey rk = bkx86.OpenSubKey(@"SOFTWARE\dkxce\device_onoff");
+                DeviceID = rk.GetValue("DeviceID", "").ToString();
+                enabled = rk.GetValue("Enabled", 0).ToString() == "1";
+                rk.Close();
+                bkx86.Close();
+                if (string.IsNullOrEmpty(DeviceID)) throw new Exception("None");
+            }
+            catch
+            { };
+        }
+
+        static void WriteRegistry(string DeviceID, bool enabled)
+        {
+            try
+            {
+                RegistryKey bkx86 = RegistryKey.OpenRemoteBaseKey(RegistryHive.CurrentUser, Environment.MachineName);
+                RegistryKey rk = bkx86.CreateSubKey(@"SOFTWARE\dkxce\device_onoff");
+                rk.SetValue("DeviceID", DeviceID);
+                rk.SetValue("Enabled", enabled ? 1 : 0);
+                rk.Close();
+                bkx86.Close();
+            }
+            catch
+            { };
+        }
+    }
+}
